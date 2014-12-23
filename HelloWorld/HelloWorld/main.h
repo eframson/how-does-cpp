f0c20cb2c595bdecc7ff82a5245638b2eb30a534
Emmett Framson <framsone@gmail.com>
2014-12-23 11:57:25 -0800

    Added functions to display a variable sized ASCII box w/text in the center, added a function which tests/illustrates pointers and references

--- HelloWorld/HelloWorld/main.h
+++ HelloWorld/HelloWorld/main.h
@@ -14,5 +14,20 @@ using namespace std;
 pair<bitset<1>, bitset<1>> simulatedFullAdder(bitset<1> a, bitset<1> b, bitset<1> carryin);
 string binaryAdder(string a, string b, bool addExtraBits = false);
 int integerAdd(int a, int b);
+void setDefaultBoxStyle(int boxStyleNum);
+void printBox(string content = "", int horizontalPadding = 2, int verticalPadding = 1, int minWidth = -1, int minHeight = -1);
+string buildHorizString(int length, char horizChar);
+string buildHorizString(long length, char horizChar);
+void refsAndPointersAndWhatnot();
+
+struct boxStyle{
+    char corner;
+    char vert;
+    char horiz;
+};
+
+boxStyle boxStyleOne = {'+','|','-'};
+boxStyle boxStyleTwo = {'*','|','='};
+boxStyle defaultBoxStyle = boxStyleOne;
 
 #endif
